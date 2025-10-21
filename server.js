2b906839780fc498ac0c778eda6e6a0255e6ed85
JuegosWeb <gd6qs6gqv9@privaterelay.appleid.com>
2025-10-21 22:01:53 +0200

    CodeBreak

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static('public'));
+
+let rooms = {};
+
+io.on('connection', (socket) => {
+  console.log('Jugador conectado:', socket.id);
+
+  socket.on('joinRoom', ({ roomId, playerName }) => {
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push({ id: socket.id, name: playerName });
+    socket.join(roomId);
+    io.to(roomId).emit('playerList', rooms[roomId]);
+  });
+
+  socket.on('playAction', ({ roomId, action }) => {
+    io.to(roomId).emit('actionUpdate', action);
+  });
+
+  socket.on('disconnect', () => {
+    Object.keys(rooms).forEach(roomId => {
+      rooms[roomId] = rooms[roomId].filter(p => p.id !== socket.id);
+      io.to(roomId).emit('playerList', rooms[roomId]);
+    });
+    console.log('Jugador desconectado:', socket.id);
+  });
+});
+
+server.listen(3000, () => console.log('Servidor iniciado en http://localhost:3000'));
+
+socket.emit('playAction', { roomId: 'sala1', action: { tipo:'pregunta', texto:'¿Cuánto suman?' } });
