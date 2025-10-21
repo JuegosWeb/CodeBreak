ca9d3053f19089d1f1491d6c134b555c39a8b932
JuegosWeb <gd6qs6gqv9@privaterelay.appleid.com>
2025-10-21 22:24:13 +0200

    empezar partida

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ let rooms = {};
 
 io.on('connection', (socket) => {
   console.log('Jugador conectado:', socket.id);
+socket.on('empezarPartida', ({ roomId }) => {
+  // Aquí repartes las fichas y envías el estado inicial a TODOS
+  io.to(roomId).emit('partidaEmpezada', { iniciado: true });
+});
 
   socket.on('joinRoom', ({ roomId, playerName }) => {
     if (!rooms[roomId]) rooms[roomId] = [];
@@ -31,6 +35,27 @@ io.on('connection', (socket) => {
     });
     console.log('Jugador desconectado:', socket.id);
   });
+});
+
+function empezarPartida() {
+  socket.emit('empezarPartida', { roomId });
+}
+
+socket.on('partidaEmpezada', ({ iniciado }) => {
+  if (iniciado) {
+    // Oculta botones y muestra la interfaz de la partida
+    document.getElementById("btnEmpezar").style.display = "none";
+    // Aquí puedes llamar a tu función de inicializar el juego, dibujar tablero, etc.
+  }
+});
+
+socket.on('playerList', players => {
+  document.getElementById('status').innerHTML = 'Jugadores conectados:<br>' + players.map(p=>p.name).join('<br>');
+  if (players.length >= 2) { // Puedes cambiar el número mínimo
+    document.getElementById("btnEmpezar").style.display = "";
+  } else {
+    document.getElementById("btnEmpezar").style.display = "none";
+  }
 });
 
 server.listen(3000, () => console.log('Servidor iniciado en http://localhost:3000'));
