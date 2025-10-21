ffaa37fc76b0e3373b4f749adcbdf53f68e653b5
JuegosWeb <gd6qs6gqv9@privaterelay.appleid.com>
2025-10-21 22:35:04 +0200

    buena

--- server.js
+++ server.js
@@ -13,16 +13,37 @@ let rooms = {};
 io.on('connection', (socket) => {
   console.log('Jugador conectado:', socket.id);
 
-  socket.on('empezarPartida', ({ roomId }) => {
-    // Aquí repartirías fichas y envías evento de inicio
-    io.to(roomId).emit('partidaEmpezada', { iniciado: true });
-  });
-
   socket.on('joinRoom', ({ roomId, playerName }) => {
     if (!rooms[roomId]) rooms[roomId] = [];
     rooms[roomId].push({ id: socket.id, name: playerName });
     socket.join(roomId);
     io.to(roomId).emit('playerList', rooms[roomId]);
+  });
+
+  socket.on('empezarPartida', ({ roomId }) => {
+    // Reparte las fichas para cada jugador
+    let fichas = [];
+    for(let n=0; n<=9; n++) {
+      if(n===5) {
+        fichas.push({numero:5, color:'verde'});
+      } else {
+        fichas.push({numero:n, color:'negro'});
+        fichas.push({numero:n, color:'blanco'});
+      }
+    }
+    fichas = fichas.sort(()=>Math.random()-0.5);
+
+    let jugadoresSala = rooms[roomId];
+    let jugadoresEstado = jugadoresSala.map(j => {
+      let mano = fichas.splice(0,5);
+      mano.sort((a,b)=>{
+        if(a.numero===b.numero) return a.color==='negro'?-1:1;
+        return a.numero-b.numero;
+      });
+      return { id:j.id, name:j.name, codigo:mano };
+    });
+
+    io.to(roomId).emit('partidaEmpezada', { jugadoresEstado });
   });
 
   socket.on('playAction', ({ roomId, action }) => {
